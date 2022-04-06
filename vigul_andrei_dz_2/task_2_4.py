f8253768100b949d1790a6d82ed30d7e417d5fb3
aovig <a.o.vigul@gmail.com>
2022-04-06 17:43:42 +0700

    vigul_andrei_dz_4

--- vigul_andrei_dz_2/task_2_4.py
+++
@@ -1,8 +0,0 @@
-def convert_name_extract(list_in: list) -> list:
-    name_list = [i.split()[-1].capitalize() for i in list_in]
-    list_out = [f'Привет, {i}!' for i in name_list]
-    return list_out
-
-my_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАЙ', 'директор аэлита']
-result = convert_name_extract(my_list)
-print(result)
